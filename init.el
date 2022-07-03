ed1a4ddb1c0b4ee3e3a194073fadff0cfffdb564
Nick Cowans <nick.cowans@gmail.com>
2022-07-03 13:12:50 +0100

    added beancount

--- init.el
+++ init.el
@@ -94,3 +94,6 @@
   :config
   (setq which-key-init-delay 0.3))
 
+(add-to-list 'load-path "~/.emacs.d/beancount-mode/")
+(require 'beancount)
+(add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
